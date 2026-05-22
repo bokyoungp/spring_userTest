afa1b9e974bac6078dd3f9f9b3163431920b0a7c
bokyoungp <bokyoungp@gmail.com>
2026-05-22 12:32:01 +0900

    content negotiation

--- src/main/java/org/example/usertest/controller/UserController.java
+++ src/main/java/org/example/usertest/controller/UserController.java
@@ -1,12 +1,13 @@
 package org.example.usertest.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.usertest.dto.UserReqDto;
 import org.example.usertest.dto.UserRespDto;
 import org.example.usertest.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,9 +17,15 @@ import java.util.List;
 public class UserController {
   private final UserService service;
 
-  @GetMapping("/users")
+  @GetMapping(value = "/users", produces = MediaType.APPLICATION_XML_VALUE)
   public List<UserRespDto> getAllUsers() {
     return service.getAllUsers();
+  }
+
+  @PostMapping("/users")
+  @ResponseStatus(HttpStatus.CREATED)
+  public UserRespDto createNewUser(@RequestBody UserReqDto reqDto) {
+    return service.createNewUser(reqDto);
   }
 
 }
--- src/main/java/org/example/usertest/service/UserService.java
+++ src/main/java/org/example/usertest/service/UserService.java
@@ -1,6 +1,7 @@
 package org.example.usertest.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.usertest.dto.UserReqDto;
 import org.example.usertest.dto.UserRespDto;
 import org.example.usertest.model.User;
 import org.example.usertest.repository.UserRepository;
@@ -23,6 +24,12 @@ public class UserService {
 
   public UserRespDto getUserById(int id) {
     User user = repository.findById(id);
+    return UserRespDto.of(user);
+  }
+
+  public UserRespDto createNewUser(UserReqDto reqDto) {
+    User user = new User(0, reqDto.userId(), reqDto.username(), reqDto.email());
+    user.setId(repository.insert(user));
     return UserRespDto.of(user);
   }
 }
